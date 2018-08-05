370351dbfc06c499fc19f4da6e91a2989c68051d
Yohanan Baruchel <ybaruchel@gmail.com>
2018-08-05 13:04:55 +0300

    Update micropay.php

--- config/micropay.php
+++ config/micropay.php
@@ -9,6 +9,6 @@ return [
 	'uid' => env('MICROPAY_UID'),
 	'su' => env('MICROPAY_SUB_USER', 0),
 	'un' => env('MICROPAY_USERNAME'),
-	'charset' => env('MICROPAY_CHARSET'),
+	'charset' => env('MICROPAY_CHARSET', 'iso-8859-8'),
 	'from' => env('MICROPAY_FROM_PHONE_NUMBER'),
 ];
